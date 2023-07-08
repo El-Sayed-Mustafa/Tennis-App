4e6a4b18f6de89ff04bfbccc9a9161e48d8e9bfd
Elsayed Mustafa <engelsayedmustafa123@gmail.com>
2023-07-09 02:54:32 +0300

    Club Screen UI (ClubInfo) 1

--- lib/Featured/club/club_screen.dart
+++ lib/Featured/club/club_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
+import 'package:tennis_app/Featured/club/widgets/club_info.dart';
 
 import '../../core/utils/widgets/app_bar_wave.dart';
 
@@ -8,14 +9,31 @@ class ClubScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: AppBarWaveHome(
-        prefixIcon: SvgPicture.asset(
-          'assets/images/profile-icon.svg',
-          fit: BoxFit.contain,
+    final screenWidth = MediaQuery.of(context).size.width;
+    final screenHeight = MediaQuery.of(context).size.height;
+
+    final sectionTitleSize = screenWidth * 0.052;
+
+    final spacing = screenHeight * 0.015;
+    return Container(
+      color: const Color(0xFFF8F8F8),
+      child: SingleChildScrollView(
+        child: Column(
+          children: [
+            AppBarWaveHome(
+              prefixIcon: SvgPicture.asset(
+                'assets/images/profile-icon.svg',
+                fit: BoxFit.contain,
+              ),
+              text: 'Your Club',
+              suffixIconPath: 'assets/images/app-bar-icon.svg',
+            ),
+            SizedBox(
+              height: spacing,
+            ),
+            ClubInfo()
+          ],
         ),
-        text: 'Your Club',
-        suffixIconPath: 'assets/images/app-bar-icon.svg',
       ),
     );
   }
