9497ad9bf9a0a589221d0bc83c94a41440b00524
Elsayed Mustafa <engelsayedmustafa123@gmail.com>
2023-07-09 23:56:49 +0300

    containers for chats

--- lib/Main-Features/chats/widgets/group.dart
+++ lib/Main-Features/chats/widgets/group.dart
@@ -5,6 +5,29 @@ class Groups extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Container(
+        width: double.infinity,
+        decoration: const ShapeDecoration(
+          color: Color(0xFFF8F8F8),
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.only(
+              topLeft: Radius.circular(30),
+              topRight: Radius.circular(30),
+            ),
+          ),
+          shadows: [
+            BoxShadow(
+              color: Color(0x33000000),
+              blurRadius: 3,
+              offset: Offset(0, -4),
+              spreadRadius: 0,
+            )
+          ],
+        ),
+      ),
+    );
+    ;
   }
 }
