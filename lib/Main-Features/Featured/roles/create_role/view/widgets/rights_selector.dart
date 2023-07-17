a945fd373fb93eee8ada983c4f5b5396486f6d18
Elsayed Mustafa <engelsayedmustafa123@gmail.com>
2023-07-17 18:36:49 +0300

    bug fix in roles screen

--- lib/Main-Features/Featured/roles/create_role/view/widgets/rights_selector.dart
+++ lib/Main-Features/Featured/roles/create_role/view/widgets/rights_selector.dart
@@ -16,15 +16,17 @@ class RightSelector extends StatefulWidget {
 
 class _RightSelectorState extends State<RightSelector> {
   List<String> words = [
-    'Apple',
-    'Banana',
-    'Cherry',
-    'Durian',
-    'Elderberry',
-    'Fig',
-    'Grape',
-    'Honeydew',
-    'Jackfruit',
+    'Send invitation',
+    'Create Events',
+    'Create tennis courts',
+    'Create offers',
+    'Edit club',
+    'Delete club',
+    'Edit members',
+    'Delete members',
+    'Create Training',
+    'Set up leagues',
+    'Create Roles',
   ];
 
   List<Color> wordColors = [
@@ -54,75 +56,92 @@ class _RightSelectorState extends State<RightSelector> {
             ),
           ),
         ),
-        Container(
-          padding: const EdgeInsets.symmetric(horizontal: 8.0, vertical: 4),
-          decoration: BoxDecoration(
-            borderRadius: BorderRadius.circular(30),
-            border: Border.all(color: const Color(0x300A557F), width: 1),
-            boxShadow: const [
-              BoxShadow(
-                color: Colors.white,
-                blurRadius: 4,
-                offset: Offset(0, 0),
-              ),
-            ],
-          ),
-          child: Row(
+        SizedBox(
+          width: double.infinity,
+          child: Stack(
             children: [
-              Expanded(
-                child: Wrap(
-                  spacing: 8.0,
-                  runSpacing: 8.0,
-                  children: widget.selectedWords.map<Widget>((String word) {
-                    return Container(
-                      decoration: ShapeDecoration(
-                        color: wordColors[widget.selectedWords.indexOf(word) %
-                            wordColors.length],
-                        shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(20),
+              LayoutBuilder(
+                builder: (context, constraints) {
+                  final containerHeight =
+                      widget.selectedWords.isNotEmpty ? null : 50.0;
+
+                  return Container(
+                    width: double.infinity,
+                    height: containerHeight,
+                    padding: const EdgeInsets.symmetric(
+                        horizontal: 8.0, vertical: 4),
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(30),
+                      border:
+                          Border.all(color: const Color(0x300A557F), width: 1),
+                      boxShadow: const [
+                        BoxShadow(
+                          color: Colors.white,
+                          blurRadius: 4,
+                          offset: Offset(0, 0),
                         ),
-                      ),
-                      padding: const EdgeInsets.all(10),
-                      margin: const EdgeInsets.symmetric(vertical: 4),
-                      child: Text(word),
-                    );
-                  }).toList(),
-                ),
+                      ],
+                    ),
+                    child: Wrap(
+                      spacing: 6.0,
+                      runSpacing: 6.0,
+                      children: widget.selectedWords.map<Widget>((String word) {
+                        return Container(
+                          decoration: ShapeDecoration(
+                            color: wordColors[
+                                widget.selectedWords.indexOf(word) %
+                                    wordColors.length],
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(20),
+                            ),
+                          ),
+                          padding: const EdgeInsets.all(10),
+                          margin: const EdgeInsets.symmetric(vertical: 4),
+                          child: Text(word),
+                        );
+                      }).toList(),
+                    ),
+                  );
+                },
               ),
-              DropdownButtonHideUnderline(
-                child: DropdownButton<String>(
-                  value: null,
-                  onChanged: (String? newValue) {
-                    setState(() {
-                      if (newValue != null) {
-                        if (widget.selectedWords.contains(newValue)) {
-                          widget.selectedWords.remove(newValue);
-                        } else {
-                          widget.selectedWords.add(newValue);
+              Positioned(
+                right: 8,
+                top: 0,
+                child: DropdownButtonHideUnderline(
+                  child: DropdownButton<String>(
+                    value: null,
+                    onChanged: (String? newValue) {
+                      setState(() {
+                        if (newValue != null) {
+                          if (widget.selectedWords.contains(newValue)) {
+                            widget.selectedWords.remove(newValue);
+                          } else {
+                            widget.selectedWords.add(newValue);
+                          }
+                          widget.onSelectedWordsChanged(widget.selectedWords);
                         }
-                        widget.onSelectedWordsChanged(widget.selectedWords);
-                      }
-                    });
-                  },
-                  style: const TextStyle(
-                    color: Colors.black,
-                    fontSize: 16,
+                      });
+                    },
+                    style: const TextStyle(
+                      color: Colors.black,
+                      fontSize: 16,
+                    ),
+                    borderRadius: BorderRadius.circular(16),
+                    icon: const Icon(
+                      Icons.arrow_drop_down,
+                      color: Color(0xFF1B262C),
+                      size: 30,
+                    ),
+                    items: words.map<DropdownMenuItem<String>>((String value) {
+                      return DropdownMenuItem<String>(
+                        value: value,
+                        child: Padding(
+                          padding: const EdgeInsets.symmetric(horizontal: 8),
+                          child: Text(value),
+                        ),
+                      );
+                    }).toList(),
                   ),
-                  borderRadius: BorderRadius.circular(16),
-                  icon: const Icon(
-                    Icons.arrow_drop_down,
-                    color: Color(0xFF1B262C),
-                    size: 30,
-                  ),
-                  items: words.map<DropdownMenuItem<String>>((String value) {
-                    return DropdownMenuItem<String>(
-                      value: value,
-                      child: Padding(
-                        padding: const EdgeInsets.symmetric(horizontal: 8),
-                        child: Text(value),
-                      ),
-                    );
-                  }).toList(),
                 ),
               ),
             ],
---
+++ lib/Main-Features/Featured/roles/roles_list/view/roles_list_screen.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class RolesScree extends StatelessWidget {
+  const RolesScree({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
