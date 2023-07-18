3e971bbd774771841a340a0773c94199283dc417
Elsayed Mustafa <engelsayedmustafa123@gmail.com>
2023-07-19 01:39:26 +0300

    player club

--- lib/Main-Features/Featured/profile/view/widgets/profile_body.dart
+++ lib/Main-Features/Featured/profile/view/widgets/profile_body.dart
@@ -3,6 +3,7 @@ import 'package:tennis_app/Main-Features/Featured/profile/view/widgets/personal_
 import 'package:tennis_app/Main-Features/Featured/profile/view/widgets/playing_info.dart';
 
 import '../../../../../generated/l10n.dart';
+import '../../../../club/widgets/club_info.dart';
 
 class ProfileBody extends StatelessWidget {
   const ProfileBody({super.key});
@@ -11,11 +12,38 @@ class ProfileBody extends StatelessWidget {
   Widget build(BuildContext context) {
     final screenHeight = MediaQuery.of(context).size.height;
     final screenWidth = MediaQuery.of(context).size.width;
-    return Padding(
-      padding: EdgeInsets.symmetric(horizontal: screenWidth * .1),
-      child: const Column(
-        children: [PersonalInfo(), SizedBox(height: 20), PlayingInfo()],
-      ),
+    return Column(
+      children: [
+        Padding(
+          padding: EdgeInsets.symmetric(horizontal: screenWidth * .07),
+          child: const Column(
+            children: [
+              PersonalInfo(),
+              SizedBox(height: 20),
+              PlayingInfo(),
+            ],
+          ),
+        ),
+        const SizedBox(height: 10),
+        const Padding(
+          padding: EdgeInsets.all(8.0),
+          child: Text(
+            'Your Club',
+            style: TextStyle(
+              color: Color(0xFF313131),
+              fontSize: 18,
+              fontFamily: 'Poppins',
+              fontWeight: FontWeight.w500,
+            ),
+          ),
+        ),
+        Container(
+            margin: EdgeInsets.only(
+                right: screenWidth * .05,
+                left: screenWidth * .05,
+                bottom: screenWidth * .05),
+            child: const ClubInfo()),
+      ],
     );
   }
 }
