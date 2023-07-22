77405dad5d15e460e0578725da1b55ee3fb434ee
Elsayed Mustafa <engelsayedmustafa123@gmail.com>
2023-07-23 00:04:59 +0300

    InviteMember backend

--- lib/Main-Features/Featured/club_managment/view/screens/invite_members.dart
+++ lib/Main-Features/Featured/club_managment/view/screens/invite_members.dart
@@ -2,7 +2,10 @@ import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 import '../../../../../core/utils/widgets/app_bar_wave.dart';
 import '../../../../../models/club.dart';
-import '../widgets/item_invite.dart';
+import '../../../../../models/player.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+import '../widgets/member_item.dart';
 
 class InviteMember extends StatelessWidget {
   const InviteMember({super.key, required this.club});
@@ -28,16 +31,62 @@ class InviteMember extends StatelessWidget {
             suffixIconPath: '',
           ),
           Expanded(
-            child: ListView.builder(
-              itemCount: 5,
-              itemBuilder: (context, index) {
-                return Container(
-                    padding: EdgeInsets.all(8.0), child: MemberItem());
+            child: FutureBuilder<List<Player>>(
+              future: getEligiblePlayers(),
+              builder: (context, snapshot) {
+                if (snapshot.connectionState == ConnectionState.waiting) {
+                  return CircularProgressIndicator();
+                } else if (snapshot.hasError) {
+                  return Text('Error: ${snapshot.error}');
+                } else {
+                  final players = snapshot.data ?? [];
+                  return ListView.builder(
+                    itemCount: players.length,
+                    itemBuilder: (context, index) {
+                      final player = players[index];
+                      return Container(
+                        padding: EdgeInsets.all(8.0),
+                        child: MemberItem(
+                          // Pass the player to the MemberItem widget
+                          member: player,
+                        ),
+                      );
+                    },
+                  );
+                }
               },
             ),
           ),
         ],
       ),
     );
+  }
+
+  Future<List<Player>> getEligiblePlayers() async {
+    try {
+      // Get all players from the Firestore "players" collection
+      final playersSnapshot =
+          await FirebaseFirestore.instance.collection('players').get();
+
+      // Filter out players who are already members of the current club
+      final existingMembers = club.memberIds;
+      final eligiblePlayers = playersSnapshot.docs
+          .map((doc) => Player.fromSnapshot(doc))
+          .where((player) => !existingMembers.contains(player.playerId))
+          .toList();
+
+      // Sort players based on the number of clubs involved in ascending order
+      eligiblePlayers.sort((a, b) =>
+          a.participatedClubIds.length.compareTo(b.participatedClubIds.length));
+
+      // Take the first ten players (or less if the list is smaller)
+      final firstTenPlayers = eligiblePlayers.take(10).toList();
+
+      return firstTenPlayers;
+    } catch (e) {
+      // Handle any errors that occur during the process
+      print('Error getting eligible players: $e');
+      return [];
+    }
   }
 }
