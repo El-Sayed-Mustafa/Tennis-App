e30f0cc45f6ef177f0faba70c35cd6eefcb5f2a4
Elsayed Mustafa <engelsayedmustafa123@gmail.com>
2023-08-17 11:42:49 +0300

    get the SingleTournamentsClub for specific club

--- lib/Main-Features/club/widgets/single_tournament_club.dart
+++ lib/Main-Features/club/widgets/single_tournament_club.dart
@@ -2,8 +2,11 @@ import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
 import 'package:tennis_app/models/single_match.dart'; // Replace with your model
 import 'package:cloud_firestore/cloud_firestore.dart';
+import '../../../core/methodes/firebase_methodes.dart';
 import '../../../core/utils/widgets/single_match_card copy.dart';
-import '../../../generated/l10n.dart'; // Replace with your model
+import '../../../generated/l10n.dart';
+import '../../../models/club.dart';
+import '../../../models/player.dart'; // Replace with your model
 
 class VerticalCarouselSlider extends StatelessWidget {
   final List<SingleMatch> matches;
@@ -41,14 +44,15 @@ class VerticalCarouselSlider extends StatelessWidget {
 }
 
 class SingleTournamentsClub extends StatelessWidget {
-  const SingleTournamentsClub({super.key});
+  const SingleTournamentsClub({Key? key});
 
   @override
   Widget build(BuildContext context) {
     final double screenHeight = MediaQuery.of(context).size.height;
     final double screenWidth = MediaQuery.of(context).size.width;
+    final double carouselHeight = (screenHeight + screenWidth) * 0.18;
 
-    final double carouselHeight = (screenHeight + screenWidth) * 0.22;
+    Method methodes = Method();
 
     return StreamBuilder<QuerySnapshot<Map<String, dynamic>>>(
       stream: FirebaseFirestore.instance
@@ -61,63 +65,107 @@ class SingleTournamentsClub extends StatelessWidget {
 
         final tournamentDocs = snapshot.data!.docs;
 
-        return Column(
-          children: [
-            const SizedBox(height: 10),
-            Text(
-              S.of(context).SingleTournaments,
-              style: const TextStyle(
-                color: Color(0xFF313131),
-                fontSize: 19,
-                fontFamily: 'Poppins',
-                fontWeight: FontWeight.w500,
-              ),
-            ),
-            CarouselSlider(
-              options: CarouselOptions(
-                height: tournamentDocs.isNotEmpty
-                    ? carouselHeight
-                    : 0, // Set height based on matches list
-                aspectRatio: 1,
-                viewportFraction: 0.7,
-                initialPage: 0,
-                enableInfiniteScroll: false,
-                enlargeCenterPage: true, autoPlayCurve: Curves.linear,
-              ),
-              items: tournamentDocs.map((tournamentDoc) {
-                final tournamentData = tournamentDoc.data();
-                if (tournamentData == null) {
-                  // Handle missing data
-                  return Container(
-                    child: const Text('sss'),
-                  ); // Return an empty container or a placeholder widget
+        return FutureBuilder<Player>(
+          future: methodes.getCurrentUser(),
+          builder: (context, userSnapshot) {
+            if (!userSnapshot.hasData) {
+              return const Center(child: CircularProgressIndicator());
+            }
+
+            final currentPlayer = userSnapshot.data!;
+            final clubId = currentPlayer.participatedClubId;
+
+            return FutureBuilder<Club>(
+              future: methodes.fetchClubData(clubId),
+              builder: (context, clubSnapshot) {
+                if (!clubSnapshot.hasData) {
+                  return const Center(child: CircularProgressIndicator());
                 }
 
-                // Fetch the subcollection data for 'singleMatches'
-                return FutureBuilder<QuerySnapshot<Map<String, dynamic>>>(
-                  future:
-                      tournamentDoc.reference.collection('singleMatches').get(),
-                  builder: (context, matchesSnapshot) {
-                    if (!matchesSnapshot.hasData) {
-                      return const Center(child: CircularProgressIndicator());
-                    }
+                final clubData = clubSnapshot.data!;
+                final singleTournamentsIds = clubData.singleTournamentsIds;
 
-                    final tournamentMatches = matchesSnapshot.data!.docs
-                        .map((matchDoc) => SingleMatch.fromFirestore(matchDoc))
-                        .toList();
+                if (singleTournamentsIds == null ||
+                    singleTournamentsIds.isEmpty) {
+                  return Container(
+                    child: const Text(
+                        'No Single Tournaments available for this club.'),
+                  );
+                }
 
-                    return Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        // Use the VerticalCarouselSlider to display matches
-                        VerticalCarouselSlider(matches: tournamentMatches),
-                      ],
-                    );
-                  },
+                final filteredTournamentDocs = tournamentDocs.where((doc) {
+                  final tournamentData = doc.data();
+                  if (tournamentData == null) {
+                    return false;
+                  }
+                  final tournamentId = doc.id;
+                  return singleTournamentsIds.contains(tournamentId);
+                }).toList();
+
+                return Column(
+                  children: [
+                    const SizedBox(height: 10),
+                    Text(
+                      S.of(context).SingleTournaments,
+                      style: const TextStyle(
+                        color: Color(0xFF313131),
+                        fontSize: 19,
+                        fontFamily: 'Poppins',
+                        fontWeight: FontWeight.w500,
+                      ),
+                    ),
+                    CarouselSlider(
+                      options: CarouselOptions(
+                        height: filteredTournamentDocs.isNotEmpty
+                            ? carouselHeight * 1.15
+                            : 0,
+                        aspectRatio: 1,
+                        viewportFraction: 0.75,
+                        initialPage: 0,
+                        enableInfiniteScroll: false,
+                        enlargeCenterPage: true,
+                        autoPlayCurve: Curves.linear,
+                      ),
+                      items: filteredTournamentDocs.map((tournamentDoc) {
+                        final tournamentData = tournamentDoc.data();
+                        if (tournamentData == null) {
+                          return Container(
+                            child: const Text('sss'),
+                          );
+                        }
+
+                        return FutureBuilder<
+                            QuerySnapshot<Map<String, dynamic>>>(
+                          future: tournamentDoc.reference
+                              .collection('singleMatches')
+                              .get(),
+                          builder: (context, matchesSnapshot) {
+                            if (!matchesSnapshot.hasData) {
+                              return const Center(
+                                  child: CircularProgressIndicator());
+                            }
+
+                            final tournamentMatches = matchesSnapshot.data!.docs
+                                .map((matchDoc) =>
+                                    SingleMatch.fromFirestore(matchDoc))
+                                .toList();
+
+                            return Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                VerticalCarouselSlider(
+                                    matches: tournamentMatches),
+                              ],
+                            );
+                          },
+                        );
+                      }).toList(),
+                    ),
+                  ],
                 );
-              }).toList(),
-            ),
-          ],
+              },
+            );
+          },
         );
       },
     );
