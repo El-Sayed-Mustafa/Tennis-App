34ce9f38fce49c9713927aa550da8aa1b22594d1
Elsayed Mustafa <engelsayedmustafa123@gmail.com>
2023-08-28 01:03:01 +0300

    handle When creating a search for a match, the name, age and address should be automatically pulled from the player and club profile and not required to be entered

--- lib/Main-Features/Featured/find_match/view/find_match_screen.dart
+++ lib/Main-Features/Featured/find_match/view/find_match_screen.dart
@@ -6,9 +6,12 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:go_router/go_router.dart';
 import 'package:tennis_app/Main-Features/Featured/find_match/view/screens/people_requirment.dart';
+import 'package:tennis_app/core/methodes/firebase_methodes.dart';
 import 'package:tennis_app/core/utils/snackbar.dart';
 import 'package:tennis_app/core/utils/widgets/custom_button.dart';
 import 'package:tennis_app/core/utils/widgets/pop_app_bar.dart';
+import 'package:tennis_app/models/club.dart';
+import 'package:tennis_app/models/player.dart';
 import '../../../../core/utils/widgets/input_date.dart';
 import '../../../../core/utils/widgets/text_field.dart';
 import '../../../../generated/l10n.dart';
@@ -58,147 +61,206 @@ class FindMatch extends StatelessWidget {
               ),
             );
           } else {
-            return BlocProvider(
-              create: (context) => ClubNamesCubit(),
-              child: Scaffold(
-                body: Container(
-                  color: const Color(0xFFF8F8F8),
-                  child: SingleChildScrollView(
-                    child: Column(
-                      children: [
-                        PoPAppBarWave(
-                          prefixIcon: IconButton(
-                            onPressed: () {
-                              GoRouter.of(context).pop();
-                            },
-                            icon: const Icon(
-                              Icons.arrow_back,
-                              size: 30,
-                              color: Colors.white,
+            Method method = Method();
+            return FutureBuilder<Player>(
+                future: method.getCurrentUser(),
+                builder: (context, snapshot) {
+                  if (snapshot.connectionState == ConnectionState.waiting) {
+                    return const Dialog.fullscreen(
+                      child: Center(
+                        child: CircularProgressIndicator(),
+                      ),
+                    );
+                  } else if (snapshot.hasError) {
+                    return Center(child: Text('Error: ${snapshot.error}'));
+                  }
+
+                  final player = snapshot.data;
+
+                  // Set player's data in controllers
+                  nameController.text = player?.playerName ?? '';
+
+                  return FutureBuilder<Club>(
+                      future: method.fetchClubData(player!.participatedClubId),
+                      builder: (context, snapshot) {
+                        if (snapshot.connectionState ==
+                            ConnectionState.waiting) {
+                          return const Dialog.fullscreen(
+                            child: Center(
+                              child: CircularProgressIndicator(),
                             ),
-                          ),
-                          text: S.of(context).Find_Match,
-                          suffixIconPath: '',
-                        ),
-                        Text(
-                          S.of(context).set_requirements,
-                          style: const TextStyle(
-                            color: Color(0xFF313131),
-                            fontSize: 20,
-                            fontFamily: 'Roboto',
-                            fontWeight: FontWeight.w500,
-                          ),
-                        ),
-                        Padding(
-                          padding: const EdgeInsets.all(20.0),
-                          child: Container(
-                            width: double.infinity,
-                            decoration: const ShapeDecoration(
-                              color: Colors.white,
-                              shape: RoundedRectangleBorder(
-                                borderRadius: BorderRadius.all(
-                                  Radius.circular(30),
-                                ),
-                              ),
-                              shadows: [
-                                BoxShadow(
-                                  color: Color(0x440D5FC3),
-                                  blurRadius: 5,
-                                  offset: Offset(0, 1),
-                                  spreadRadius: 0,
-                                )
-                              ],
-                            ),
-                            child: Form(
-                              key: formKey,
+                          );
+                        } else if (snapshot.hasError) {
+                          return Center(
+                              child: Text('Error: ${snapshot.error}'));
+                        }
+
+                        final club = snapshot.data;
+                        addressController.text = club!.address;
+                        return Scaffold(
+                          body: Container(
+                            color: const Color(0xFFF8F8F8),
+                            child: SingleChildScrollView(
                               child: Column(
                                 children: [
-                                  SizedBox(height: screenHeight * .025),
-                                  ProfileImage(
-                                    onImageSelected: (File imageFile) {
-                                      _selectedImageBytes =
-                                          imageFile.readAsBytesSync();
-                                    },
-                                  ),
-                                  SizedBox(height: screenHeight * .025),
-                                  InputTextWithHint(
-                                    hint: S.of(context).typeYourName,
-                                    text: S.of(context).playerName,
-                                    controller: nameController,
-                                  ),
-                                  SizedBox(height: screenHeight * .025),
-                                  InputTextWithHint(
-                                    hint: S.of(context).Type_Club_Address_here,
-                                    text: S.of(context).Club_Address,
-                                    controller: addressController,
-                                  ),
-                                  SizedBox(height: screenHeight * .025),
-                                  InputDate(
-                                    hint: S.of(context).Select_Date_of_Birth,
-                                    text: S.of(context).Your_Age,
-                                    onDateTimeSelected: (DateTime dateTime) {
-                                      // Handle date selection
-                                    },
-                                  ),
-                                  SizedBox(height: screenHeight * .025),
-                                  InputTimeField(
-                                    hint: S
-                                        .of(context)
-                                        .typeYourPreferredPlayingTime,
-                                    text: S.of(context).preferredPlayingTime,
-                                    onTimeSelected: (TimeOfDay? time) {
-                                      _selectedTime = time;
-                                    },
-                                  ),
-                                  SizedBox(height: screenHeight * .025),
-                                  const PlayerType(),
-                                  SizedBox(height: screenHeight * .015),
-                                  SizedBox(height: screenHeight * .03),
-                                  Padding(
-                                    padding: const EdgeInsets.all(8.0),
-                                    child: BottomSheetContainer(
-                                      buttonText: S.of(context).Find,
-                                      onPressed: () async {
-                                        if (formKey.currentState!.validate()) {
-                                          // Retrieve the necessary data from the form and other widgets
-                                          DateTime? selectedDateTime =
-                                              context.read<DateCubit>().state;
-
-                                          String? selectedPlayerType = context
-                                              .read<PlayerTypeCubit>()
-                                              .state;
-                                          Uint8List? selectedImageBytes =
-                                              _selectedImageBytes;
-                                          User? user =
-                                              FirebaseAuth.instance.currentUser;
-
-                                          context
-                                              .read<FindMatchCubit>()
-                                              .saveData(
-                                                user!.uid.toString(),
-                                                nameController,
-                                                addressController,
-                                                selectedDateTime,
-                                                _selectedTime,
-                                                selectedPlayerType,
-                                                selectedImageBytes,
-                                                context,
-                                              );
-                                        }
+                                  PoPAppBarWave(
+                                    prefixIcon: IconButton(
+                                      onPressed: () {
+                                        GoRouter.of(context).pop();
                                       },
+                                      icon: const Icon(
+                                        Icons.arrow_back,
+                                        size: 30,
+                                        color: Colors.white,
+                                      ),
                                     ),
-                                  )
+                                    text: S.of(context).Find_Match,
+                                    suffixIconPath: '',
+                                  ),
+                                  Text(
+                                    S.of(context).set_requirements,
+                                    style: const TextStyle(
+                                      color: Color(0xFF313131),
+                                      fontSize: 20,
+                                      fontFamily: 'Roboto',
+                                      fontWeight: FontWeight.w500,
+                                    ),
+                                  ),
+                                  Padding(
+                                    padding: const EdgeInsets.all(20.0),
+                                    child: Container(
+                                      width: double.infinity,
+                                      decoration: const ShapeDecoration(
+                                        color: Colors.white,
+                                        shape: RoundedRectangleBorder(
+                                          borderRadius: BorderRadius.all(
+                                            Radius.circular(30),
+                                          ),
+                                        ),
+                                        shadows: [
+                                          BoxShadow(
+                                            color: Color(0x440D5FC3),
+                                            blurRadius: 5,
+                                            offset: Offset(0, 1),
+                                            spreadRadius: 0,
+                                          )
+                                        ],
+                                      ),
+                                      child: Form(
+                                        key: formKey,
+                                        child: Column(
+                                          children: [
+                                            SizedBox(
+                                                height: screenHeight * .025),
+                                            ProfileImage(
+                                              onImageSelected:
+                                                  (File imageFile) {
+                                                _selectedImageBytes =
+                                                    imageFile.readAsBytesSync();
+                                              },
+                                            ),
+                                            SizedBox(
+                                                height: screenHeight * .025),
+                                            InputTextWithHint(
+                                              hint: S.of(context).typeYourName,
+                                              text: S.of(context).playerName,
+                                              controller: nameController,
+                                            ),
+                                            SizedBox(
+                                                height: screenHeight * .025),
+                                            InputTextWithHint(
+                                              hint: S
+                                                  .of(context)
+                                                  .Type_Club_Address_here,
+                                              text: S.of(context).Club_Address,
+                                              controller: addressController,
+                                            ),
+                                            SizedBox(
+                                                height: screenHeight * .025),
+                                            InputDate(
+                                              hint: S
+                                                  .of(context)
+                                                  .Select_Date_of_Birth,
+                                              text: S.of(context).Your_Age,
+                                              onDateTimeSelected:
+                                                  (DateTime dateTime) {
+                                                // Handle date selection
+                                              },
+                                            ),
+                                            SizedBox(
+                                                height: screenHeight * .025),
+                                            InputTimeField(
+                                              hint: S
+                                                  .of(context)
+                                                  .typeYourPreferredPlayingTime,
+                                              text: S
+                                                  .of(context)
+                                                  .preferredPlayingTime,
+                                              onTimeSelected:
+                                                  (TimeOfDay? time) {
+                                                _selectedTime = time;
+                                              },
+                                            ),
+                                            SizedBox(
+                                                height: screenHeight * .025),
+                                            const PlayerType(),
+                                            SizedBox(
+                                                height: screenHeight * .015),
+                                            SizedBox(
+                                                height: screenHeight * .03),
+                                            Padding(
+                                              padding:
+                                                  const EdgeInsets.all(8.0),
+                                              child: BottomSheetContainer(
+                                                buttonText: S.of(context).Find,
+                                                onPressed: () async {
+                                                  if (formKey.currentState!
+                                                      .validate()) {
+                                                    // Retrieve the necessary data from the form and other widgets
+                                                    DateTime? selectedDateTime =
+                                                        context
+                                                            .read<DateCubit>()
+                                                            .state;
+
+                                                    String? selectedPlayerType =
+                                                        context
+                                                            .read<
+                                                                PlayerTypeCubit>()
+                                                            .state;
+                                                    Uint8List?
+                                                        selectedImageBytes =
+                                                        _selectedImageBytes;
+                                                    User? user = FirebaseAuth
+                                                        .instance.currentUser;
+
+                                                    context
+                                                        .read<FindMatchCubit>()
+                                                        .saveData(
+                                                          user!.uid.toString(),
+                                                          nameController,
+                                                          addressController,
+                                                          selectedDateTime,
+                                                          _selectedTime,
+                                                          selectedPlayerType,
+                                                          selectedImageBytes,
+                                                          context,
+                                                        );
+                                                  }
+                                                },
+                                              ),
+                                            )
+                                          ],
+                                        ),
+                                      ),
+                                    ),
+                                  ),
                                 ],
                               ),
                             ),
                           ),
-                        ),
-                      ],
-                    ),
-                  ),
-                ),
-              ),
-            );
+                        );
+                      });
+                });
           }
         },
       ),
